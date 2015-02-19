1ce45c77c316453823dd065b3435b263843fd824
Jernej Kovacic <jkovacic@gmail.com>
2015-02-19 15:17:49 +0100

    video inpainting using median

--- week7/task.m
+++ week7/task.m
@@ -13,3 +13,23 @@ it = img_inpainting_i_t(img);
 disp('Figure 2: inpainting PDEs'' dI/dt');
 figure(2);
 mesh(it);
+
+
+rep = input('Press any key to continue...', 's');
+close all;
+
+% Video inpainting using the median of consecutive frames
+
+% Animated GIF downloaded from:
+% https://commons.wikimedia.org/wiki/File:SimpleAnimation.gif
+
+v=imread('../SimpleAnimation.gif', 'frames', 'all');
+vmed = median_inpaint(v,4);
+
+disp('First video player: the original video');
+implay(double(v), 1);
+disp('Second video player: processed video with hthe white section inpainted');
+implay(double(vmed), 1);
+
+
+disp('Video players must be closed manually');
